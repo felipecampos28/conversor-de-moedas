2de8344abf5d494dc3a47114c97337ab150c6130
felipecampos28 <felipe.dev.28@gmail.com>
2024-07-24 15:53:28 -0300

    Codando JS

---
+++ script.js
@@ -0,0 +1,62 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  // Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value")  // Valor em outras moedas
+
+    console.log(currencySelect.value)
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+
+
+    if (currencySelect == dolar) {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect == euro) {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+    if (currencySelect == libra) {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect == bitcoin) {
+
+        CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+
+    // CAMPO REAL PARA CONVERTER
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+
+
+
+
+
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
+
+
+
+
